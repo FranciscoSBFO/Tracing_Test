3769be8b3c4113607d4d9fb76d9536aa832b7f81
Francisco <sbfo180617@upemor.edu.mx>
2022-03-03 14:12:31 -0600

    TracingTest

--- src/main/java/com/demo/controller/operacion/SolicitudServicioClienteController.java
+++ src/main/java/com/demo/controller/operacion/SolicitudServicioClienteController.java
@@ -20,7 +20,7 @@ import com.demo.service.operacion.SolicitudServicioClienteMuestrasService;
 import com.demo.service.operacion.SolicitudServicioClienteService;
 import com.demo.utils.Constantes;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+//import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
